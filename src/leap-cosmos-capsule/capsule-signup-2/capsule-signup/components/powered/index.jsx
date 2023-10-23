3feea14a57bf452310f9ca559a01d335d85f0a75
SamvelRaja Sakthivel <sam@SamvelRajas-MacBook-Pro.local>
2023-10-23 17:11:20 +0530

    Added powered by logo

--- src/leap-cosmos-capsule/capsule-signup-2/capsule-signup/components/powered/index.jsx
+++ src/leap-cosmos-capsule/capsule-signup-2/capsule-signup/components/powered/index.jsx
@@ -1,11 +1,33 @@
+import Image from "next/image";
+import LeapSVG from '../Icon/Leap.svg';
+import CapsuleSVG from '../Icon/Capsule.svg';
+
 const PoweredBy = () => {
-    return <div style={{
+    return <div 
+    style={{
         color: "#D6D6D6",
         fontSize: '16px',
         fontWeight: 500,
         lineHeight: '16px',
-        marginBottom: '1rem'
-    }} > Powered by Leap x Capsule </div>
+    }} > Powered by 
+    <Image 
+        style={{
+            marginLeft: '10px',
+            marginBottom: '10px'
+        }} 
+        alt="capsule" 
+        src={CapsuleSVG} 
+    /> 
+        <div> X </div> 
+        <Image 
+            style={{
+                marginLeft: '10px',
+                marginBottom: '5px'
+            }} 
+            alt="leap" 
+            src={LeapSVG} 
+        /> 
+    </div>
 }
 
 export default PoweredBy;
